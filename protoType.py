101671e2d0848542f245aea8ea12f89a56c2cee8
Barbod <76158078+Barbod1380@users.noreply.github.com>
2025-04-25 20:04:14 +0330

    Add Joint-by-Joint plot.

--- protoType.py
+++ protoType.py
@@ -239,7 +239,8 @@ def create_unwrapped_pipeline_visualization(defects_df, joints_df):
                         thickness=20,
                         len=0.6,
                         y=0.5,
-                        yanchor="middle"
+                        yanchor="middle",
+                        titleside="right"
                     ),
                     opacity=0.8,
                     line=dict(width=1, color='white')
